2f11c59623da1cd8aa84e5005ce7244326b5923e
Philipp Schunker <philipp@schunker.at>
2019-03-27 10:19:25 +0100

    added MCDiffTreeNode.h in .framework headers

---
+++ MonteCarloKit.framework/Headers/MCDiffTreeNode.h
@@ -0,0 +1,20 @@
+//
+//  MCDiffTreeNode.h
+//  MonteCarloKit
+//
+//  Created by Philipp Schunker on 25.03.19.
+//  Copyright © 2019 Philipp Schunker. All rights reserved.
+//
+
+#import <MonteCarloKit/MonteCarloKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MCDiffTreeNode : MCTreeNode
+
+- (nonnull id<NSObject>)diffIdentifier;
+- (BOOL)isEqualToDiffableObject:(id)object;
+
+@end
+
+NS_ASSUME_NONNULL_END
